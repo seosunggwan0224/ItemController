07cb7765999f891d64dbc3118fcfffc8290b7514
seosunggwan <59163966+seosunggwan0224@users.noreply.github.com>
2024-05-12 23:54:13 +0900

    second commit

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -22,7 +22,7 @@ public class ItemController {
         List<Item> result = itemRepository.findAll();
         System.out.println(result.get(0).price);
         System.out.println(result.get(0).title);
-//        List<Object> a = new ArrayList<>();
+        List<Object> a = new ArrayList<>();
 
         model.addAttribute("items", result);
 
